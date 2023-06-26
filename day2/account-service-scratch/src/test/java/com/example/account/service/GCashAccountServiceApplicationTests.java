f472a7b71c956df079f15ed928f554ef05f67814
n-yeung <nathan-r.yeung@gcash.com>
2023-06-26 16:43:18 +0800

    day 2 scratch

---
+++ day2/account-service-scratch/src/test/java/com/example/account/service/GCashAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.account.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GCashAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
